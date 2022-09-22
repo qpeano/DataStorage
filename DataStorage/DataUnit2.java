bdaf8b6f6dde16da6b451f893beb0a085a45d78e
Shamiur <85860293+qpeano@users.noreply.github.com>
2022-09-22 18:38:22 +0200

    check 35

---
+++ DataStorage/DataUnit2.java
@@ -0,0 +1,39 @@
+package data;
+import java.util.ArrayList;
+
+public class DataUnit {
+
+    private ArrayList<DataUnit> innerUnits;
+    private ArrayList<String> fragments;
+    private DataUnit outerUnit;
+    private String label;
+
+    public DataUnit(String name) {
+
+        this.label = this.formatLabel(name);
+        this.outerUnit = null;
+        this.fragments = new ArrayList<>();
+        this.innerUnits = new ArrayList<>();
+    }
+
+    private String formatLabel(String name) {
+
+        String newName = name.replaceAll( "\\{", "").replaceAll( "    ", "");
+        return newName;
+    }
+
+    private String formatFragment(String info) {
+
+        String newInfo = info.replaceAll("    ", ""); // removes tab
+        return newInfo;
+    }
+
+    public DataUnit newInnerUnit(String label, /* DataUnit outerUnit */) {
+
+        String formattedLabel = this.formatLabel(label);
+        DataUnit innerUnit = new DataUnit(formattedLabel);
+        innerUnit.outerUnit = this; // does this work?
+        this.innerUnits.add(innerUnit);
+        return innerUnit;
+    }
+}
