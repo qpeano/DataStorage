749167449e76f90f248fd8c624a4da08ff32141e
Shamiur <85860293+qpeano@users.noreply.github.com>
2022-10-07 09:27:25 +0200

    untested version of toString() added
    uses an indirect recursive approach. every inner unit calls their toString,.

--- DataStorage/DataUnit2.java
+++ DataStorage/DataUnit2.java
@@ -35,6 +35,7 @@ public class DataUnit {
         // innerUnit.outerUnit = this; // does this work?
         innerUnit.outerUnit = this.getThisUnit();
         this.innerUnits.add(innerUnit);
+        this.fragment.add(null); // assigns space for unit in file
         return innerUnit;
     }
 
@@ -46,5 +47,57 @@ public class DataUnit {
     public DataUnit getOuterUnit() {
 
         return this.outerUnit;
+    }
+
+    public void add(String fragment) {
+
+        String newFragment = this.formatFragment(fragment);
+        this.fragments.add(newFragment);
+    }
+
+    public String toString() {
+
+        StringBuilder content = new StringBuilder();
+        String formedLabel = this.label + " {\n";
+        content.append(formedLabel);
+        int indexOfInnerUnitList = 0;
+
+        for (int count = 0; count < this.fragments.size(); count++) {
+
+            String fragment = this.fragments.get(count);
+            if (fragment == null) {
+
+                String[] arrayOfElements = this.innerUnits.get(indexOfInnerUnitList).toString().split("\n");
+                for (int j = 0; j < arrayOfElements.length; j++) {
+
+                    String formattedLine;
+                    if (j == arrayOfElements.length - 1) {
+
+                        formattedLine = "   " + arrayOfElements[j] + "\n}";
+                    }
+                    else {
+
+                        formattedLine = "   " + arrayOfElements[j] + "\n";
+                    }
+                    content.append(formattedLine);
+                }
+                indexOfInnerUnitList++;
+            }
+            else {
+
+                String formattedFragment;
+                if (count == this.fragments.size() - 1) {
+
+                    formattedFragment =  "    " + fragment;
+                }
+                else {
+
+                    String formattedFragment =  "    " + fragment + "\n";
+                }
+                content.append(formattedFragment);
+            }
+        }
+
+        return content.toString();
     }
 }
